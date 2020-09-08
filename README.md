8c01064ad2a5519b5ac0cfe3ec237a5a156744c4
Christian Linne <ShadowVampire@web.de>
2020-09-08 13:13:57 +0200

    Attempting to get unit tests to work ... somehow

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Just a playground app tot figure out how good Svelte works in conjunction with T
 # Known issues
 
 - ESLint in `.svelte` files does not work in conjunction with TypeScript yet, since a preprocessor is required. See https://github.com/sveltejs/eslint-plugin-svelte3/pull/62<br>As such, `eslint-plugin-svelte3` will not be used for now.
+
+- Testing does not work atm. due to the way `tailwindcss` has to be imported to not get removed automatically. Comment the line `import "./TailwindSetup.svelte";` in `App.svelte` to execute the unit test properly. Related issue for `svelte-jester`: https://github.com/mihar-22/svelte-jester/issues/20<br>
+Note that `jest-transform-svelte` did not work either with or without importing the `tailwindcss` setup.
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   ...require("@snowpack/app-scripts-svelte/jest.config.js")(),
-  collectCoverageFrom: [
-    "src/**/*.{ts,svelte}",
-    "!src/**/*.{spec,test}.ts",
-  ],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.{spec,test}.ts"],
   coverageReporters: ["json", "lcov", "cobertura", "text"],
   coverageDirectory: "<rootDir>/test-results/coverage",
   coveragePathIgnorePatterns: ["/node_modules/", ".d.ts"],
   reporters: ["default", "jest-junit"],
   testEnvironment: "jsdom",
   testURL: "http://localhost",
+  transform: {
+    "^.+\\.svelte$": [
+      "svelte-jester",
+      {
+        preprocess: true,
+      },
+    ],
+    "^.+\\.(js|ts)$":
+      "<rootDir>/node_modules/@snowpack/app-scripts-svelte/jest/babelTransform.js",
+  },
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$"],
 };
